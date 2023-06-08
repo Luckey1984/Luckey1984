a2b25b03eeebae854c4a3093f65746cf106e236a
陈大欠 <ChenDaqian@users.noreply.github.com>
2023-06-08 12:03:17 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const thisYear = new Date().getFullYear()
 const startTimeOfThisYear = new Date(`${thisYear}-01-01T00:00:00+00:00`).getTime()
 const endTimeOfThisYear = new Date(`${thisYear}-12-31T23:59:59+00:00`).getTime()
@@ -15,5 +14,8 @@ function generateProgressBar() {
 }
 
 const readme = `\
+### Hi there 👋
 ⏳ Year progress ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(2)} %
+---
 ⏰ Updated on ${new Date().toUTCString()}
+---
